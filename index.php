2ef3905df9c3d003acbef5943ad60927813471cf
Sherif Tariq <sherif.tariq@gmail.com>
2018-09-15 14:21:57 -0500

    Merged design thinking branch into master.

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<?php 
-    $currentSitePage = 'home'; 
-?>
+<?php $currentSitePage = 'home'; ?>
 <?php include 'shared/header.php' ?>
 <?php include 'pages/home/index.php' ?>
 <?php include 'shared/footer.php' ?>
