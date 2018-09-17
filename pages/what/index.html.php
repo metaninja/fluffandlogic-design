6b69555257e2058fc94fea99f95110b8624bc7d6
Sherif Tariq <sherif.tariq@gmail.com>
2018-09-17 06:07:38 -0500

    Added Why page in new Slack inspired layout.

--- pages/what/index.html.php
+++ pages/what/index.html.php
@@ -1,75 +1,86 @@
-    <header class="page-header--inside">
-        <div class="section-content--page-body">
-            <h2 class="page-header--inside__title">
-                <strong>What is Design Thinking?</strong>
-            </h2>
+<div class="page-what">
 
-            <p class="page-header--inside__desc">
-                Design thinking is an approach to problem solving that starts by gathering user data,
-                bringing together a team and then methodically defining the problem, interating on a
-                solution, and validating that solution with real users.
-            </p>
-        </div>
-    </header>
+    <h1>What is Design Thinking?</h1>
+    <p>
+        Design thinking is an approach to problem solving that starts by gathering user data, bringing together a team and
+        then methodically defining the problem, interating on a solution, and validating that solution with real users.
+    </p>
 
-    <section class="page-body__section page-body__section-what">
-        <div class="section-content--page-body">
-            <h2>The Problem</h2>
-            <p>
-                Traditionally, companies that follow the software development lifecycle (SDLC)
-                approach to product design tend to take this approach:
-            </p>
+    <object class="bg-gray" style="width: 100%; height: 400px; margin: 72px 0;"></object>
 
-            <section class="page-body__sidebar">
-                <ol>
-                    <li>Ask the customer what features they want.</li>
-                    <li>Pass the customer request to a business analyst who then writes up the requirements.</li>
-                    <li>Hand the requirements to the developers.</li>
-                    <li>Wait 6-9 months (usually longer) to deliver a beta product.</li>
-                    <li>Go through weeks of QA testing and minor bug fixes.</li>
-                    <li>Go to production.</li>
-                    <li>When customers inevitably find problems with the product, hire support people to surface
-                        the source of the problem and bring it to the developers.</li>
-                    <li>Repeat step 1.</li>
-                </ol>
-            </section>
+    <h3>Traditionally, enterprise companies approach to product design used the following approach:</h3>
 
-            <h2>Solving For The Symptoms</h2>
+    <ol class="page-steps">
+        <li>
+            <object class="placeholder-icon"></object>
+            Ask the customer what features they want.
+        </li>
+        <li>
+            <object class="placeholder-icon"></object>
+            Pass the customer request to a business analyst who then writes up the requirements.
+        </li>
+        <li>
+            <object class="placeholder-icon"></object>
+            Hand the requirements to the developers.
+        </li>
+        <li>
+            <object class="placeholder-icon"></object>
+            Wait 6-9 months (usually longer) to deliver a beta product.
+        </li>
+        <li>
+            <object class="placeholder-icon"></object>
+            Go through weeks of QA testing and minor bug fixes.
+        </li>
+        <li>
+            <object class="placeholder-icon"></object>
+            Go to production.
+        </li>
+        <li>
+            <object class="placeholder-icon"></object>
+            When customers inevitably find problems with the product, hire support people to surface the source of the problem and bring it to the developers.
+        </li>
+        <li>
+            <object class="placeholder-icon"></object>
+            Repeat step 1.
+        </li>
 
-            <div class="two-col">
-                <p>The problem with this approach is that we’re assuming the customer actually knows what they want.
-                    What we’ve found from user interviews and extensive user testing is that, <strong>users have a difficult
-                        time elucidating the problem</strong>; they can describe the symptoms of the problem, but without observing
-                    them in action, taking that as a feature list is putting band aid on the customer’s problem rather than
-                    digging into the root of the problem and fixing it.
-                </p>
+    </ol>
 
-                <p>
-                    In addition, with each release cycle, developers layer on even more and more code that over time
-                    becomes difficult to update, slowing down the release cycle even further.
-                </p>
-            </div>
-        </div>
-    </section>
 
-    <section class="page-body__conclusion">
-        <div class="section-content--page-body">
+    <h3>The problem with this approach is that we’re assuming the customer always knows what they want.</h3>
 
-            <h2>Enter Design Thinking</h2>
+    <div class="two-col">
+        <p>
+            What we’ve found from user interviews and extensive user testing is that, users have a difficult time expressing
+            the problem; they can describe the symptoms of the problem, but without observing them in action, taking that as a
+            feature list is putting band aid on the customer’s problem rather than digging into the root of the problem and
+            fixing it.
+        </p>
 
-            <p class="two-col">
-                With Design Thinking, the Product team is responsible for talking to the customer. But instead of
-                asking for a list of features to build, the team asks the customer to explain step-by-step what
-                they’re doing currently that’s so problematic and ideally capture the customer’s emotions and
-                thought process at each step. The team then goes back and analyzes the customer journey, surfaces
-                opportunities and pain potins
+        <p>
+            What we’ve found from user interviews and extensive user testing is that, users have a difficult time expressing
+            the problem; they can describe the symptoms of the problem, but without observing them in action, taking that as a
+            feature list is putting band aid on the customer’s problem rather than digging into the root of the problem and
+            fixing it.
+        </p>
+    </div>
 
-            </p>
-        </div>
-    </section>
+    <p>&nbsp;</p>
 
-    <section class="page-footer page-footer--home">
-        <a href="/pages/why/" class="page-footer__link">
-            Next: Why Design Thinking?
-        </a>
-    </section>
+    <h3>Enter Design Thinking</h3>
+
+    <p>
+        With Design Thinking, the Product team is responsible for talking to the customer. But instead of asking for a
+        list of features to build, the team asks the customer to explain step-by-step what they’re doing currently that’s so
+        problematic and ideally capture the customer’s emotions and thought process at each step. The team then goes back
+        and analyzes the customer journey, surfaces opportunities and pain points.
+    </p>
+
+    <p style="height: 80px;">&nbsp;</p>
+
+    <a href="/pages/why/" class="btn-page-footer" style="margin: 0">Next: Why Design Thinking?</a>
+
+    <p style="height: 160px;">&nbsp;</p>
+
+
+</div>
